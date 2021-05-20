71160e9b7f3330947b7e21c0e590f7bce8c05fd0
moamen_mohasseb <moamen_mohasseb@yahoo.com>
2021-05-20 14:51:47 +0200

    adding query

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
 class ListContacts extends Component {
-  state ={
-    username: 'Tyler'
+  static propType ={
+    contacts: PropTypes.array.isRequired,
+    onDeletecontact:PropTypes.func.isRequired,
 
   }
+  state ={
+               query : ''
+  }
+  updatequery = (query)=>{
+    this.setState(()=>({
+      query : query.trim()
+    }))
+  }
     render() {
-      console.log('Props', this.props)
+     const {contacts,onDeletecontact} = this.props
+     const {query}= this.state
+     const ShowingContacts = query === ''
+     ?contacts
+     :contacts.filter((c) =>
+           c.name.toLowerCase().includes(query.toLowerCase())
+     )
+      //console.log('Props', this.props)
+
       return (
+    <div className="list-contacts">   
+      <div className="list-contacts-top">
+        <input
+        type="text"
+        value={query}
+        onChange={(event)=> this.updatequery(event.target.value)}></input>
+         <p>{JSON.stringify(query)}</p>
+      </div>
     <ol className='contact-list'>
-        { this.props.contacts.map((contact) => (
+        { ShowingContacts.map((contact) => (
         <li key={contact.id} className='contact-list-item'>
             <div
               className='contact-avatar'
@@ -20,12 +47,12 @@ class ListContacts extends Component {
               <p>{contact.name}</p>
               <p>{contact.handle}</p>
             </div>
-            <button className='contact-remove' onClick={ () =>this.props.onDeletecontact(contact)}>
+            <button className='contact-remove' onClick={() =>this.props.onDeletecontact(contact)}>
               Remove
             </button>
           </li>
         ))}
-     </ol>)
+     </ol></div>)
     }
 }
 export default ListContacts
